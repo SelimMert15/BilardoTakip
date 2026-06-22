963b3a20c617c3193f3185133e64fb95463b8e9d
SelimMert15 <bestriogrup@gmail.com>
2026-06-22 12:46:45 +0300

    render duzeltmesi

--- app.py
+++ app.py
@@ -559,4 +559,5 @@ if __name__ == "__main__":
                 db.session.add(urun)
 
             db.session.commit()
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
